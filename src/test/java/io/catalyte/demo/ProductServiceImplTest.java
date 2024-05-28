122e65ea46c94b4d73120cecfa105224fd380174
William Kirk <wkirk@catalyte.io>
2024-05-28 14:30:17 -0500

    Removing unused import in ServiceImplTest

--- src/test/java/io/catalyte/demo/ProductServiceImplTest.java
+++ src/test/java/io/catalyte/demo/ProductServiceImplTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
