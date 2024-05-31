2491d29144e5dc643671fad5413753df6398afa7
Shaddy Abdou <sabdou@catalyte.io>
2024-05-31 12:31:00 -0400

    added getProductByName to Controller

--- src/main/java/io/catalyte/demo/products/ProductsController.java
+++ src/main/java/io/catalyte/demo/products/ProductsController.java
@@ -4,15 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -41,6 +33,18 @@ public class ProductsController {
   @ResponseStatus(HttpStatus.OK)
   public List<Product> getProducts() {
     return productService.getProducts();
+  }
+
+  /**
+   * Retrieves a product by its name.
+   * Exact matches only.
+   * @param name The name of the product to retrieve.
+   * @return The product(s) with the specified name.
+   */
+  @GetMapping("/filter")
+  @ResponseStatus(HttpStatus.OK)
+  public List<Product> getProductByName(@RequestParam String name) {
+    return productService.getProductByName(name);
   }
 
   /**
