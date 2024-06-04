69e8e6325e1e3be449f06bbe562b7e7cdf342ae1
Angelica Boiciuc <aboiciuc@catalyte.io>
2024-06-04 17:33:33 +0000

    Runners 8 api vendor validation2

--- src/main/java/io/catalyte/demo/vendor/vendorEntity/PhoneNumberFormatter.java
+++ src/main/java/io/catalyte/demo/vendor/vendorEntity/PhoneNumberFormatter.java
@@ -19,12 +19,6 @@ public class PhoneNumberFormatter {
         String digits = phoneNumber.replaceAll("-",
                 "");
 
-        // Check if there are 10 digits
-        if (digits.length() != 10) {
-            throw new IllegalArgumentException(
-                    "Phone number must have 10 digits and may only include dashes.");
-        }
-
         // Format the phone number as xxx-xxx-xxxx
         return digits.replaceAll(
                 "(\\d{3})(\\d{3})(\\d{4})",
