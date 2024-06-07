24589da916f3cec78b0e7fe44c13d9186548088e
William Kirk <wkirk@catalyte.io>
2024-06-07 14:43:23 -0500

    Added basic Type/Classification Validation

--- src/main/java/io/catalyte/demo/products/ProductValidator.java
+++ src/main/java/io/catalyte/demo/products/ProductValidator.java
@@ -1,5 +1,8 @@
 package io.catalyte.demo.products;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ProductValidator {
     public String validateProductActive(Product productToValidate) {
         return null;
@@ -8,10 +11,10 @@ public class ProductValidator {
     public String validateProductDescription(Product productToValidate) {
         if (productToValidate.getDescription() == null) {
             return "Description is null.";
-        } else if (productToValidate.getDescription().length() > 100) {
-            return "Description must be less than 100 characters.";
         } else if (productToValidate.getDescription().isEmpty()) {
             return "Description is empty.";
+        } else if (productToValidate.getDescription().length() > 100) {
+            return "Description must be less than 100 characters.";
         }
         return "";
     }
@@ -19,10 +22,10 @@ public class ProductValidator {
     public String validateProductName(Product productToValidate) {
         if (productToValidate.getName() == null) {
             return "Name is null.";
-        } else if (productToValidate.getName().length() > 50) {
-            return "Name must be less than 50 characters.";
         } else if (productToValidate.getName().isEmpty()) {
             return "Name is empty.";
+        } else if (productToValidate.getName().length() > 50) {
+            return "Name must be less than 50 characters.";
         }
         return "";
     }
@@ -33,6 +36,38 @@ public class ProductValidator {
 
     public String validateProductIngredientList(Product productToValidate) {
         return null;
+    }
+
+    public String validateProductClassification(Product productToValidate) {
+        if (productToValidate.getClassification() == null) {
+            return "Classification is null.";
+        } else if (productToValidate.getClassification().isEmpty()) {
+            return "Classification is empty.";
+        } else if (productToValidate.getClassification().equals("Drink") || productToValidate.getClassification().equals("Baked Good")) {
+            return "";
+        }
+        return "Classification must be Drink or Baked Good.";
+    }
+
+    public String validateProductType(Product productToValidate) {
+        List<String> drinkTypes = Arrays.asList(
+                "Coffee",
+                "Tea",
+                "Soda"
+        );
+
+        if (productToValidate.getType() == null) {
+            return "Type is null.";
+        } else if (productToValidate.getType().isEmpty()) {
+            return "Type is empty.";
+        } else {
+            for (String drink : drinkTypes) {
+                if (productToValidate.getType().equals(drink)) {
+                    return "";
+                }
+            }
+        }
+        return "Type must be Coffee, Tea, or Soda.";
     }
 
     public String validateProductCost(Product productToValidate) {
