c9e1c5548675dfccb5b4b821f50c763eed021da0
Kelsey <kbelanger@catalyte.io>
2024-06-24 16:10:40 -0400

    create ingredient controller and service files

---
+++ src/main/java/io/catalyte/demo/ingredient/IngredientController.java
@@ -0,0 +1,36 @@
+package io.catalyte.demo.ingredient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/ingredients")
+public class IngredientController {
+    /**
+     * A controller class to map CRUD functions from IngredientService to RESTful endpoints
+     * Autowired to IngredientServiceImpl (service class)
+     * */
+
+    IngredientService ingredientService;
+
+    /**
+     * @param ingredientService - the service for performing CRUD methods on Customer instances
+     * */
+    @Autowired
+    public IngredientController(IngredientService ingredientService) {
+        this.ingredientService = ingredientService;
+    }
+
+    /**
+     * Creates a new product in the repository
+     * @param ingredientToCreate - Ingredient Object containing unique identifier, active status, name,
+     *                           vendorId, ingredientsList, classification, cost, allergenList, and salePrice
+     * @return the created ingredient
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Ingredient createIngredient(@RequestBody Ingredient ingredientToCreate) {
+        return ingredientService.createIngredient(ingredientToCreate);
+    }
+}
---
+++ src/main/java/io/catalyte/demo/ingredient/IngredientService.java
@@ -0,0 +1,19 @@
+package io.catalyte.demo.ingredient;
+
+public interface IngredientService {
+
+    // CRUD methods:
+    // Commented out code = placeholder for future CRUD methods
+
+//    List<Ingredient> getIngredients();
+
+//    Ingredient getIngredientById(int id);
+
+//    List<Ingredient> getIngredientByName(String name);
+
+    Ingredient createIngredient(Ingredient ingredientToCreate);
+
+//    Ingredient editIngredient(Ingredient ingredientToEdit, int id);
+
+//    void deleteIngredientById(int id);
+}
