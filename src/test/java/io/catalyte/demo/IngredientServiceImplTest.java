fb4a6c744bebaa506d7c1fe8f3f8e4230bc092f9
Kelsey <kbelanger@catalyte.io>
2024-06-25 11:27:00 -0400

    add unit test for ingredient creation

--- src/test/java/io/catalyte/demo/IngredientServiceImplTest.java
+++ src/test/java/io/catalyte/demo/IngredientServiceImplTest.java
@@ -5,13 +5,19 @@ import io.catalyte.demo.ingredient.IngredientRepository;
 import io.catalyte.demo.ingredient.IngredientService;
 import io.catalyte.demo.ingredient.IngredientServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+//import java.util.*; // Imports all Java Util classes
 import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.util.AssertionErrors.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
 public class IngredientServiceImplTest {
@@ -23,7 +29,7 @@ public class IngredientServiceImplTest {
     Ingredient testIngredient;
 
     @BeforeEach
-    public void testIngredientParameters() {
+    public void setUp() {
         List<String> sampleAllergenList = Arrays.asList(
                 "Nuts",
                 "Gluten"
@@ -31,5 +37,19 @@ public class IngredientServiceImplTest {
 
         ingredientService = new IngredientServiceImpl(ingredientRepository);
         testIngredient = new Ingredient(1,true,"Test Ingredient", BigDecimal.valueOf(15.50),10,"lb", sampleAllergenList);
+    }
+
+    @Test
+    public void createIngredient_withAllParametersNoValidation_PersistIngredient() {
+        when(ingredientRepository.save(testIngredient)).thenReturn(testIngredient);
+
+        Ingredient result = ingredientService.createIngredient(testIngredient);
+
+        assertEquals("Test Ingredient", result.getName());
+        assertEquals(BigDecimal.valueOf(15.50), result.getPurchasingCost());
+        assertEquals(true, result.getActive());
+        assertEquals(10, result.getAmount());
+        assertEquals("lb", result.getUnitOfMeasure());
+        assertEquals(Arrays.asList("Nuts", "Gluten"), result.getAllergens());
     }
 }
