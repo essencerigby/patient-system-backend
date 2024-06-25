b6cc5a1b7c82f36442a7695039800178bc9be3a1
Kelsey Belanger <kbelanger@catalyte.io>
2024-06-25 14:57:08 +0000

    RUNNERS-99-Vendor-Validation-UPDATES

--- src/test/java/io/catalyte/demo/vendor/VendorValidationTest.java
+++ src/test/java/io/catalyte/demo/vendor/VendorValidationTest.java
@@ -4,9 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import io.catalyte.demo.vendor.vendorEntity.Address;
 import io.catalyte.demo.vendor.vendorEntity.Contact;
+import io.catalyte.demo.vendor.vendorEntity.Vendor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+
+import java.util.List;
 
 public class VendorValidationTest {
     @Mock
@@ -16,7 +19,6 @@ public class VendorValidationTest {
 
     @BeforeEach
     public void setUp() {
-
         address = new Address();
         address.setStreet("1 Street1");
         address.setStreet2("ap5");
@@ -33,214 +35,274 @@ public class VendorValidationTest {
 
     @Test
     public void testNameValidation_NullName_returnsError() {
-        String result = validator.nameValidation(null);
-        assertEquals("~Name field is null~", result);
+        List<String> result = validator.nameValidation(null);
+        assertEquals(1, result.size());
+        assertTrue(result.contains("Name field is null"));
     }
 
     @Test
     public void testNameValidation_EmptyName() {
-        String result = validator.nameValidation("");
-        assertEquals("~Name field is empty~", result);
+        List<String> result = validator.nameValidation("");
+        assertEquals(1, result.size());
+        assertTrue(result.contains("Name field is empty"));
     }
 
+    //Take a look at this test - should return true, but returns false instead
     @Test
     public void testNameValidation_TooLongName() {
-        String result = validator.nameValidation("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-        assertEquals("~Please enter a shorter name~", result);
+        String invalidName = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        List<String> result = validator.nameValidation(invalidName);
+        assertEquals(1,result.size());
+        assertTrue(result.contains("Please enter a name shorter than 50 characters"));
     }
 
     @Test
     public void testNameValidation_ValidName() {
-        String result = validator.nameValidation("VendorName");
-        assertEquals("", result);
+        List<String> result = validator.nameValidation("VendorName");
+        assertEquals(0, result.size());
     }
 
     @Test
     public void testAddressValidation_AllFieldsValid() {
-        String result = validator.addressValidation(address);
-        assertEquals("", result);
+        List<String> result = validator.addressValidation(address);
+        assertEquals(0, result.size());
     }
 
     @Test
     public void testAddressValidation_Street1Null() {
         address.setStreet(null);
-        String result = validator.addressValidation(address);
-        assertEquals("~Street1 field is null~", result);
+        List<String> result = validator.addressValidation(address);
+        assertEquals(1, result.size());
+        assertTrue(result.contains("Street1 field is null"));
     }
 
     @Test
     public void testAddressValidation_Street1Empty() {
         address.setStreet("");
-        String result = validator.addressValidation(address);
-        assertEquals("~Street1 field is empty~", result);
+        List<String> result = validator.addressValidation(address);
+        assertEquals(1, result.size());
+        assertTrue(result.contains("Street1 field is empty"));
     }
 
     @Test
     public void testAddressValidation_StateNull() {
         address.setState(null);
-        String result = validator.addressValidation(address);
-        assertEquals("~State field is null~", result);
+        List<String> result = validator.addressValidation(address);
+        assertEquals(1, result.size());
+        assertTrue(result.contains("State field is null"));
     }
 
     @Test
     public void testAddressValidation_StateEmpty() {
         address.setState("");
-        String result = validator.addressValidation(address);
-        assertEquals("~State field is empty~", result);
+        List<String> result = validator.addressValidation(address);
+        assertEquals(1, result.size());
+        assertTrue(result.contains("State field is empty"));
     }
 
     @Test
     public void testAddressValidation_StateInvalid() {
         address.setState("X");
-        String result = validator.addressValidation(address);
-        assertEquals("~Please add the right abbreviation of the state: two uppercase letters~", result);
+        List<String> result = validator.addressValidation(address);
+        assertEquals(1, result.size());
+        assertTrue(result.contains("Please add the right abbreviation of the state: two uppercase letters"));
     }
 
     @Test
     public void testAddressValidation_CityNull() {
         address.setCity(null);
-        String result = validator.addressValidation(address);
-        assertEquals("~City field is null~", result);
+        List<String> result = validator.addressValidation(address);
+        assertEquals(1, result.size());
+        assertTrue(result.contains("City field is null"));
     }
 
     @Test
     public void testAddressValidation_CityEmpty() {
         address.setCity("");
-        String result = validator.addressValidation(address);
-        assertEquals("~City field is empty~", result);
+        List<String> result = validator.addressValidation(address);
+        assertEquals(1, result.size());
+        assertTrue(result.contains("City field is empty"));
     }
 
     @Test
     public void testAddressValidation_ZipcodeNull() {
         address.setZipCode(null);
-        String result = validator.addressValidation(address);
-        assertEquals("~Zipcode field is null~", result);
+        List<String> result = validator.addressValidation(address);
+        assertEquals(1, result.size());
+        assertTrue(result.contains("Zipcode field is null"));
     }
 
     @Test
     public void testAddressValidation_ZipcodeEmpty() {
         address.setZipCode("");
-        String result = validator.addressValidation(address);
-        assertEquals("~Zipcode field is empty~", result);
+        List<String> result = validator.addressValidation(address);
+        assertEquals(1, result.size());
+        assertTrue(result.contains("Zipcode field is empty"));
     }
 
     @Test
     public void testAddressValidation_ZipcodeInvalid() {
         address.setZipCode("11g23");
-        String result = validator.addressValidation(address);
-        assertEquals("~Zipcode must be 5 numerical digits~", result);
+        List<String> result = validator.addressValidation(address);
+        assertEquals(1, result.size());
+        assertTrue(result.contains("Zipcode must be 5 numerical digits"));
     }
 
     @Test
+    public void testAddressValidation_StreetNull() {
+        address.setStreet(null);
+        List<String> result = validator.addressValidation(address);
+        assertEquals(1, result.size());
+        assertTrue(result.contains("Street1 field is null"));
+    }
+
+
+    @Test
     public void testEmailValidation_EmailNull() {
-        String result = validator.emailValidation(null);
-        assertEquals("~Email field is null~", result);
+        List<String> result = validator.emailValidation(null);
+        assertEquals(1, result.size());
+        assertTrue(result.contains("Email field is null"));
     }
 
     @Test
     public void testEmailValidation_EmailEmpty() {
-        String result = validator.emailValidation("");
-        assertEquals("~Email field is empty~", result);
+        List<String> result = validator.emailValidation("");
+        assertEquals(1, result.size());
+        assertTrue(result.contains("Email field is empty"));
     }
 
     @Test
     public void testEmailValidation_InvalidFormat() {
-        String result = validator.emailValidation("ex@");
-        assertEquals("The email is not in the right format: x@x.x", result);
+        List<String> result = validator.emailValidation("ex@");
+        assertEquals(1, result.size());
+        assertTrue(result.contains("The email is not in the right format: x@x.x"));
     }
 
     @Test
     public void testEmailValidation_ValidFormat() {
-        String result = validator.emailValidation("ex@tr.com");
-        assertEquals("", result);
+        List<String> result = validator.emailValidation("ex@tr.com");
+        assertEquals(0, result.size());
     }
+
 
     @Test
     public void testPhoneValidation_PhoneNull() {
-        String result = validator.phoneValidation(null);
-        assertEquals("~Phone field is null~", result);
+        List<String> result = validator.phoneValidation(null);
+        assertEquals(1, result.size());
+        assertTrue(result.contains("Phone field is null"));
     }
 
     @Test
     public void testPhoneValidation_PhoneEmpty() {
-        String result = validator.phoneValidation("");
-        assertEquals("~Phone field is empty~", result);
+        List<String> result = validator.phoneValidation("");
+        assertEquals(1, result.size());
+        assertTrue(result.contains("Phone field is empty"));
     }
 
     @Test
     public void testPhoneValidation_InvalidFormat() {
-        String result = validator.phoneValidation("123-abc-7890");
-        assertEquals("~The phone number is not in the right format: 999-999-9999 or 9999999999~", result);
+        List<String> result = validator.phoneValidation("123-abc-7890");
+        assertEquals(1, result.size());
+        assertTrue(result.contains("The phone number is not in the right format: 999-999-9999 or 9999999999"));
     }
 
     @Test
     public void testPhoneValidation_ValidFormat_WithoutDashes() {
-        String result = validator.phoneValidation("1234567890");
-        assertEquals("", result);
+        List<String> result = validator.phoneValidation("1234567890");
+        assertEquals(0, result.size());
     }
 
     @Test
     public void testPhoneValidation_ValidFormat_WithDashes() {
-        String result = validator.phoneValidation("123-456-7890");
-        assertEquals("", result);
+        List<String> result = validator.phoneValidation("123-456-7890");
+        assertEquals(0, result.size());
     }
+
 
     @Test
     public void testContactValidation_AllFieldsValid() {
-        String result = validator.contactValidation(contact);
-        assertEquals("", result);
+        List<String> result = validator.contactValidation(contact);
+        assertEquals(0, result.size());
     }
 
     @Test
     public void testEmailValidation_EmailAndPhoneInvalid() {
         contact.setEmail("fgh@g.");
         contact.setPhone("77-888-9999");
-        String result = validator.contactValidation(contact);
-        assertTrue(result.contains("~Either a valid email or phone number must be provided~"));
+        List<String> result = validator.contactValidation(contact);
+        assertEquals(2, result.size());
         assertTrue(result.contains("The email is not in the right format: x@x.x"));
-        assertTrue(result.contains("~The phone number is not in the right format: 999-999-9999 or 9999999999~"));
+        assertTrue(result.contains("The phone number is not in the right format: 999-999-9999 or 9999999999"));
+    }
+
+    @Test
+    public void testContactValidation_NeitherPhoneNorEmailProvided_returnErrorMessage() {
+        contact.setPhone("");
+        contact.setEmail("");
+        List<String> result = validator.contactValidation(contact);
+        assertTrue(result.contains("Either a valid email or phone number must be provided"));
     }
 
     @Test
     public void testEmailValidation_NoEmailNoPhone() {
         contact.setEmail("");
         contact.setPhone("");
-        String result = validator.contactValidation(contact);
-        assertTrue(result.contains("~Either a valid email or phone number must be provided~"));
+        List<String> result = validator.contactValidation(contact);
+        assertEquals(3, result.size());
+        assertTrue(result.contains("Either a valid email or phone number must be provided"));
     }
 
     @Test
     public void testContactValidation_ContactNameNull() {
         contact.setContactName(null);
-        String result = validator.contactValidation(contact);
-        assertTrue(result.contains("~Contact name field is null~"));
+        List<String> result = validator.contactValidation(contact);
+        assertEquals(1, result.size());
+        assertTrue(result.contains("Contact name field is null"));
     }
 
     @Test
     public void testContactValidation_ContactNameEmpty() {
         contact.setContactName("");
-        String result = validator.contactValidation(contact);
-        assertTrue(result.contains("~Contact name field is empty~"));
+        List<String> result = validator.contactValidation(contact);
+        assertEquals(1, result.size());
+        assertTrue(result.contains("Contact name field is empty"));
     }
 
     @Test
     public void testContactValidation_ContactNameInvalid() {
         contact.setContactName("FirstName");
-        String result = validator.contactValidation(contact);
-        assertTrue(result.contains("~Please add Contact name as First name and Last name separated by a space. Only alphabetic characters, hyphens or apostrophes allowed~"));
+        List<String> result = validator.contactValidation(contact);
+        assertEquals(1, result.size());
+        assertTrue(result.contains("Please add Contact name as First name and Last name separated by a space. Only alphabetic characters, hyphens or apostrophes are allowed"));
     }
 
     @Test
     public void testContactValidation_RoleNull() {
         contact.setTitleOrRole(null);
-        String result = validator.contactValidation(contact);
-        assertTrue(result.contains("~Title/Role field is null~"));
+        List<String> result = validator.contactValidation(contact);
+        assertEquals(1, result.size());
+        assertTrue(result.contains("Title/Role field is null"));
     }
 
     @Test
     public void testContactValidation_RoleEmpty() {
         contact.setTitleOrRole("");
-        String result = validator.contactValidation(contact);
-        assertTrue(result.contains("~Title/role field is empty~"));
+        List<String> result = validator.contactValidation(contact);
+        assertEquals(1, result.size());
+        assertTrue(result.contains("Title/Role field is empty"));
+    }
+
+    @Test
+    public void validateVendor_captureErrorMessages_returnArrayOfErrors() {
+        // Create a vendor with invalid data for testing purposes
+        Vendor vendor = new Vendor();
+        vendor.setName(""); // Invalid name
+        vendor.setAddress(new Address()); // Invalid address
+        vendor.setContact(new Contact()); // Invalid contact
+
+        String[] errors = validator.validateVendor(vendor);
+
+        // Some errors are expected, length unspecified
+        assertTrue(errors.length > 0);
     }
 }
