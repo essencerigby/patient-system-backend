fa7d75e6459004de6868bf51699911835a77149f
Kelsey <kbelanger@catalyte.io>
2024-06-27 09:43:23 -0400

    add name validation method

--- src/main/java/io/catalyte/demo/ingredient/IngredientValidator.java
+++ src/main/java/io/catalyte/demo/ingredient/IngredientValidator.java
@@ -2,6 +2,8 @@ package io.catalyte.demo.ingredient;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Formats ingredient amount to be stored with two decimal places
@@ -11,5 +13,23 @@ public class IngredientValidator {
         BigDecimal bd = new BigDecimal(amount);
         bd = bd.setScale(2, RoundingMode.HALF_UP);
         return bd.toString();
+    }
+
+    /**
+     * Validates an ingredient's name.
+     *
+     * @param name the name to be validated
+     * @return a list of error messages if the name is invalid; otherwise, an empty list
+     */
+    public List<String> nameValidation(String name) {
+        List<String> errors = new ArrayList<>();
+        if (name == null) {
+            errors.add("Name field is null");
+        } else if (name.isBlank()) {
+            errors.add("Name field is empty");
+        } else if (name.length() >= 50) {
+            errors.add("Please enter an ingredient name shorter than 50 characters");
+        }
+        return errors;
     }
 }
