8855da014cbe8083fde1caa72d6338c75e24d933
Essence Rigby <erigby@catalyte.io>
2024-07-18 14:23:18 -0400

    added DC to states list

--- src/main/java/io/catalyte/demo/patients/PatientValidation.java
+++ src/main/java/io/catalyte/demo/patients/PatientValidation.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class PatientValidation {
     final List<String> US_states = Arrays.asList("AL", "AK", "AZ", "AR",
-            "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY",
+            "CA", "CO", "CT", "DC", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY",
             "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
             "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
             "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY");
